cf88b3a29901087b45323f04153eaa54cf44dac9
apoorvnema <apoorvnema38@gmail.com>
2024-03-31 23:21:26 +0530

    Fetching user details

--- index.js
+++ index.js
@@ -1,56 +1,17 @@
+const ul = document.querySelector("ul");
 function handleFormSubmit(event) {
     event.preventDefault();
-    const userDetails = {
-        username: event.target.username.value,
-        email: event.target.email.value,
-        phone: event.target.phone.value,
-    };
-    axios
-        .post(
-            "https://crudcrud.com/api/c9af40cbb821453ab02d1fd2a5492066/appointmentData",
-            userDetails
-        )
-        .then((response) => displayUserOnScreen(response.data))
-        .catch((error) => console.log(error));
-
-    // Clearing the input fields
-    document.getElementById("username").value = "";
-    document.getElementById("email").value = "";
-    document.getElementById("phone").value = "";
+    axios.get("https://crudcrud.com/api/c9af40cbb821453ab02d1fd2a5492066/appointmentData").then(res => {
+        for (let i = 0; i < res.data.length; i++) {
+            printUsers(res.data[i])
+        }
+    }).catch(err => console.log(err));
+}
+function printUsers(user) {
+    const li = document.createElement("li");
+    li.textContent = user.username + "-" + user.email + "-" + user.phone;
+    ul.appendChild(li);
 }
 
-function displayUserOnScreen(userDetails) {
-    const userItem = document.createElement("li");
-    userItem.appendChild(
-        document.createTextNode(
-            `${userDetails.username} - ${userDetails.email} - ${userDetails.phone}`
-        )
-    );
-
-    const deleteBtn = document.createElement("button");
-    deleteBtn.appendChild(document.createTextNode("Delete"));
-    userItem.appendChild(deleteBtn);
-
-    const editBtn = document.createElement("button");
-    editBtn.appendChild(document.createTextNode("Edit"));
-    userItem.appendChild(editBtn);
-
-    const userList = document.querySelector("ul");
-    userList.appendChild(userItem);
-
-    deleteBtn.addEventListener("click", function (event) {
-        userList.removeChild(event.target.parentElement);
-        localStorage.removeItem(userDetails.email);
-    });
-
-    editBtn.addEventListener("click", function (event) {
-        userList.removeChild(event.target.parentElement);
-        localStorage.removeItem(userDetails.email);
-        document.getElementById("username").value = userDetails.username;
-        document.getElementById("email").value = userDetails.email;
-        document.getElementById("phone").value = userDetails.phone;
-    });
-}
-
-// Do not touch code below
+// Do not touch the code below
 module.exports = handleFormSubmit;
